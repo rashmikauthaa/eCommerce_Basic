44fed3b912b09897ade8d990f9aa5382429b2293
Rashmika <autharush@gmail.com>
2025-02-14 15:57:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+    <h1>heyy</h1>
+    <button>Noraml Button</button> <br /> <br />
+    <Button>Bootstrap wala</Button>
+    </>
+  )
+}
+
+export default App
